19cbbe6170f7999e128a8b937231202dacef51af
Nadezhda10Dev <nadiaolegivna10@gmail.com>
2025-08-27 13:55:08 +0300

    Update script.js
    Copy func

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 console.log('Сайт запущен!');
 console.log('The site was created with the support of Murka Technologies');
+function copyText(text) {
+  navigator.clipboard.writeText(text)
+    .then(() => alert("Скопировано: " + text))
+    .catch(err => console.error("Ошибка копирования: ", err));
+}
